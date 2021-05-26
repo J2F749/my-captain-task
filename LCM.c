7411037021e118dc807f1cb226aae877fbe97085
J2F749 <84285194+J2F749@users.noreply.github.com>
2021-05-26 11:19:23 +0530

    Update LCM.c

--- LCM.c
+++ LCM.c
@@ -7,24 +7,21 @@ int main()
 	printf("Enter 1st number:");
 	scanf("%d",&a);
 	printf("Enter 2nd number:");
-    scanf("%d",&b);
-    printf("The LCM of %d and %d is %d",a,b,max);
-    (max=(a>b)?a:b);
-	return 0;
+        scanf("%d",&b);
+        printf("The LCM of %d and %d is %d",a,b,max);
+        return 0;
 }
 int find_lcm(int a, int b, int max)
 {
 	if(max%a==0 && max%b==0)
-    {
-	  
-	return 0;  
+        {
+	  return (max=(a>b)?a:b);  
 	}
 	else
 	{
 	  return ++max; 
-    }
-    return 0;
-	
+        }
+        return 0;
 }
 
     
